c3e69cffe5124c0971221223d0e4cb3a3db4d89b
Namcu1998 <namcu1998@gmail.com>
2021-11-24 05:45:23 +0700

    fix

--- find_and_move_video.bat
+++ find_and_move_video.bat
@@ -23,7 +23,8 @@ echo "find and move to derectory memory stick"
 for ((i1=0 ; i1 < ${#folderScan[@]} ; i1++));
 do
     if [ -d "${folderScan[$i1]}" ];
-    then                                                              cd ${folderScan[$i1]}                                         echo "scanning image in folder ${folderScan[$i1]}"
+    then                                                              cd "${folderScan[$i1]}" 
+echo "scanning image in folder ${folderScan[$i1]}"
         for ((i2=0 ; i2 < ${#DirectoryPath[@]} ; i2++));
         do                                                                sudo find -name "*.mp4" -exec mv {} ${storageMemoryStick}/${DirectoryPath[$i2]} \;
         done
