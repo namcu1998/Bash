8f1b67466ea4881c473e71e9649f8c1024ef9b44
namcu1998 <45044341+namcu1998@users.noreply.github.com>
2023-03-01 17:40:23 +0700

    Update sort_files_in_desktop.sh

--- sort_files_in_desktop.sh
+++ sort_files_in_desktop.sh
@@ -12,21 +12,29 @@ render_line() {
 	done
 }
 
-mapfile -d '' list_file < <(find -maxdepth 1 -type f ! -name "sort.sh" -print0)
+mapfile -d '' list_file < <(find -maxdepth 1  ! -name "sort.sh" ! -name "root*" -print0)
 
 for ((i=0;i<${#list_file[@]};i++)) ; do
-	file_date=$(date +__%m%y -r "${list_file[i]}"  | tr ' ' '\n' | head -n 2 | tail -1)
-	file_type=$(echo "${list_file[i]}" | tr '.' '\n' | tail -1)
-	file_name=$(echo "${list_file[i]}" | tr '/' '\n' | tail -1)
-	printf "%s\n" $(echo "${list_file[i]}" | tr '.' '\n' | tail -1)
+	if [[ ${list_file[i]} != "." ]] ; then #Check folder isn't point
+	
+		if [[ -d ${list_file[i]} ]] ; then #Check that is value folder or file?
+			echo "yes ${list_file[i]}"
+		else
+			file_date=$(date +__%m%y -r "${list_file[i]}"  | tr ' ' '\n' | head -n 2 | tail -1)
+			file_type=$(echo "${list_file[i]}" | tr '.' '\n' | tail -1)
+			file_name=$(echo "${list_file[i]}" | tr '/' '\n' | tail -1)
+			printf "%s\n" $(echo "${list_file[i]}" | tr '.' '\n' | tail -1)
 
-	if ! [[ -d ${root}/${file_date}/${file_type} ]] ; then
-		echo -e "the folder doesn't exist\nCreate ${root}/${file_date}/${file_type} folder\n"
-		mkdir -p ${root}/${file_date}/${file_type}
+			if ! [[ -d ${root}/${file_date}/${file_type} ]] ; then
+				echo -e "the folder doesn't exist\nCreate ${root}/${file_date}/${file_type} folder\n"
+				mkdir -p ${root}/${file_date}/${file_type}
+			fi
+			
+			echo -e "\n${file_name}\t${root}/${file_date}/${file_type}\t$(date -r "${list_file[i]}")" >> ${root}/log.txt
+			
+			mv "${list_file[i]}" ${root}/${file_date}/${file_type}
+			render_line >> ${root}/log.txt
+		fi
 	fi
 	
-	echo -e "\n${file_name}\t${root}/${file_date}/${file_type}\t$(date -r "${list_file[i]}")" >> ${root}/log.txt
-	
-	mv "${list_file[i]}" ${root}/${file_date}/${file_type}
-	render_line >> ${root}/log.txt
 done
